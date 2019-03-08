c820ab0b861b8970d5f1cf1ec6a4bf9fd3704840
loki3789 <40861505+loki3789@users.noreply.github.com>
2019-03-08 20:05:57 +0530

    Update index.php

--- contactManagement/index.php
+++ contactManagement/index.php
@@ -17,12 +17,12 @@ $config=new Config();
 $contacts = $contact->allContacts();
 
 if (isset($_POST['name'])) {
-    $requested_name = $_POST['name'];
-    $contacts = ($contact->contactsByName($requested_name) != []) ? $contact->contactsByName($requested_name) : $contacts;
+        $requested_name = $_POST['name'];
+        $contacts = ($contact->contactsByName($requested_name) != []) ? $contact->contactsByName($requested_name) : $contacts;
 }
 $pages = $config->pages(sizeof($contacts));
 if (isset($_GET['p'])) {
-    $paginate = (((integer) $_GET['p'] == 0) || ((integer) $_GET['p'] > $pages)) ? 1 : (integer) $_GET['p'];
+        $paginate = (((integer) $_GET['p'] == 0) || ((integer) $_GET['p'] > $pages)) ? 1 : (integer) $_GET['p'];
 }
 $contacts = $config->refill($paginate, $contacts);
 include(realpath($_SERVER["DOCUMENT_ROOT"]) . '\contactManagement\app\views\mainPage.php');
